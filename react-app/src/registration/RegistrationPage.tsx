4f4339708e1c1ce53e744cdcdac07670e0a0007a
Hans Sowa <hans.sowa@polygons.at>
2024-05-24 15:51:56 +0200

    Add backend registration call

--- react-app/src/registration/RegistrationPage.tsx
+++ react-app/src/registration/RegistrationPage.tsx
@@ -1,9 +1,29 @@
-import RegistrationForm from "@/registration/RegistrationForm.tsx";
-import {Container} from "@mui/material";
+import RegistrationForm, {UserPwdLoginFormFields} from "@/registration/RegistrationForm.tsx";
+import {Box, Container, Typography} from "@mui/material";
+import {useState} from "react";
 
 const RegistrationPage = () => {
+
+    const [message, setMessage] = useState<string>("");
+
+    const registrationHandler = (fields: UserPwdLoginFormFields) => {
+        fetch("/api/register", {
+            method: 'POST',
+            body: JSON.stringify(fields)
+        }).then((response) => {
+            if (response.ok) {
+                setMessage('Benutzer:in wurde erfolgreich registriert');
+            }
+        });
+    }
+
     return <Container>
-        <RegistrationForm  onAction={() => {}}/>
+        <RegistrationForm  onAction={registrationHandler}/>
+        <Box mt="1rem" mb="1rem">
+            <Typography align={'inherit'} component="p" color={'primary'}>
+                {message}
+            </Typography>
+        </Box>
     </Container>
 }
 
